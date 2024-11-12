da54eca239f66e873965aeae41d0d54922d91877
mohamedvpn <103552750+mohamedvpn@users.noreply.github.com>
2024-11-12 17:18:22 +0200

    Update FB-Checker.py

--- FB-Checker.py
+++ FB-Checker.py
@@ -2,5 +2,5 @@
   "J8K2-WQ4R-TY7U-9PL3-XZ5V-1NM6-A2SD-FG3H":{'name':'Eslam','end_date':'11/16/2024'},
   "G5H2-MNTC-UTZ7-IXV4-DLBC-8R1X-1GW6-03T7":{'name':'Wesam','end_date':'11/16/2024'},
   "G5H2-MNTC-UTZ7-IXV4-DLBC-8R1X-1GW6-3333":{'name':'Ali','end_date':'11/19/2024'},
-  "G5H23-M4NTC-UTZ57-IXV64-D33LBC":{'name':'Ali','end_date':'11/19/2027'},
+  "G5H23-M4NTC-UTZ57-IXV64-D33LBC":{'name':'Commander Ali Abdel Salam','end_date':'11/19/2027'},
 }
